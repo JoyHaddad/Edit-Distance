5b2293a854a0e92207b9e455eb070e4c4063d56f
JoyHaddad <joy4haddad@gmail.com>
2023-06-12 15:14:07 -0700

    modify file

--- edit-distance.py
+++ edit-distance.py
@@ -1,26 +1,41 @@
-def minDistance(x: str, y: str) -> int:
-    m = len(x)
-    n = len(y)
-    # dp[i][j] := min # Of operations to convert word1[0..i) to word2[0..j)
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
+def minDistance(x, y):
+    m, n = len(x), len(y)
+    
+    dp_chart = []
+    for _ in range(m + 1):
+        row = [0] * (n + 1)
+        dp_chart.append(row)
+
 
     for i in range(1, m + 1):
-      dp[i][0] = i
+        dp_chart[i][0] = i
 
     for j in range(1, n + 1):
-      dp[0][j] = j
+        dp_chart[0][j] = j
+
+    operations = []
 
     for i in range(1, m + 1):
-      for j in range(1, n + 1):
-        if x[i - 1] == y[j - 1]:
-          dp[i][j] = dp[i - 1][j - 1]
-        else:
-          dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+        for j in range(1, n + 1):
+            if x[i - 1] == y[j - 1]:
+                dp_chart[i][j] = dp_chart[i - 1][j - 1]
+            else:
+                dp_chart[i][j] = min(dp_chart[i - 1][j - 1], dp_chart[i - 1][j], dp_chart[i][j - 1]) + 1
+                if dp_chart[i][j] == dp_chart[i - 1][j - 1] + 1:
+                    operations.append(f"Substitute \"{x[i - 1]}\" into \"{y[j - 1]}\"")
+                elif dp_chart[i][j] == dp_chart[i - 1][j] + 1:
+                    operations.append(f"Delete \"{x[i - 1]}\"")
+                elif dp_chart[i][j] == dp_chart[i][j - 1] + 1:
+                    operations.append(f"Insert \"{y[j - 1]}\"")
 
-    return dp[m][n]
+    for op in operations:
+        print(op)
 
-# Example usage
-x = "clarks"
-y = "kirk"
+    return dp_chart[m][n]
+
+# User input for x and y
+x = input("Enter the first word: ")
+y = input("Enter the second word: ")
+
 min_distance = minDistance(x, y)
 print(f"Minimum edit distance: {min_distance}")
